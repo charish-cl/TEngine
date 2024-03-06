24bb32c8072519c72ebb3542ffae0eb5da4521c3
codeelk <chaomengop@163.com>
2024-03-06 20:06:15 +0800

    贪吃蛇

---
+++ UnityProject/Assets/Scripts/Main.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using FEngine;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Main : MonoSingleton<Main>
+{
+    
+    public int width = 100;
+    public int height = 100;
+    
+    
+    [LabelText("食物生成间隔")]
+    public float interval = 0.1f;
+    
+    [LabelText("食物预制体")]
+    public GameObject FoodPrefab; 
+    [LabelText("蛇预制体")]
+    public GameObject SnakePrefab;
+    [LabelText("蛇身预制体")]
+    public GameObject SnakeSegmentPrefab;
+    
+    
+    Snake Snake;
+    void Start()
+    {
+         //生成蛇
+         Snake = Instantiate(SnakePrefab, Vector3.zero, Quaternion.identity).GetComponent<Snake>();
+            
+         //用UniTask每隔interval秒生成一个食物
+         GenerateFood();
+         
+    }
+
+   
+
+    private async void GenerateFood()
+    {
+        while (true)
+        {
+            await UniTask.Delay((int)(interval * 1000));
+            var x = Random.Range(-width / 2, width / 2);
+            var y = Random.Range(-height / 2, height / 2);
+            Instantiate(FoodPrefab, new Vector3(x, y, 0), Quaternion.identity);
+            
+        }
+    }
+
+    
+    private void OnDrawGizmos()
+    {
+        //绘制地图边界
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(new Vector3(-width / 2, -height / 2, 0), new Vector3(width / 2, -height / 2, 0));
+        Gizmos.DrawLine(new Vector3(-width / 2, height / 2, 0), new Vector3(width / 2, height / 2, 0));
+        Gizmos.DrawLine(new Vector3(-width / 2, -height / 2, 0), new Vector3(-width / 2, height / 2, 0));
+        Gizmos.DrawLine(new Vector3(width / 2, -height / 2, 0), new Vector3(width / 2, height / 2, 0));
+    }
+}
---
+++ UnityProject/Assets/Scripts/Snake.cs
@@ -0,0 +1,116 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using FEngine;
+using UnityEngine;
+
+public class Snake:MonoBehaviour
+{
+    
+    //蛇身
+    public List<SnakeSegment> Segments=new List<SnakeSegment>();
+    //蛇移动速度
+    public float Speed = 1;
+
+    //蛇尾偏移
+    public float TailOffset = 3f;
+    
+    public Vector2 Direction;
+    
+    public Vector3 MousePos;
+    
+    public List<Vector3> Positions=new List<Vector3>();
+    public List<float> Directions=new List<float>();
+    
+    private void Update()
+    {
+        //获取鼠标位置,舌头朝向鼠标位置
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        MousePos = new Vector3(mousePos.x, mousePos.y, 0);
+        Direction = (mousePos - transform.position).normalized;
+        
+        var width = Main.Instance.width;
+        var height = Main.Instance.height;
+        //检测是否越界,如果越界游戏结束
+        if (transform.position.x < -width / 2 || transform.position.x > width / 2 || transform.position.y < -height / 2 || transform.position.y > height / 2)
+        {
+            Debug.Log("Game Over");
+            Destroy(gameObject);
+        }
+    }
+    
+    private void FixedUpdate()
+    {
+        //移动蛇
+        transform.LookAt2DY(MousePos);
+        
+        Directions.Add(transform.eulerAngles.z);
+        
+        Direction = (MousePos - transform.position).normalized;
+        
+        var newPosition=transform.position+new Vector3(Direction.x,Direction.y,0)*Speed*Time.fixedDeltaTime;
+        transform.position = newPosition;
+        Positions.Add(newPosition);
+        
+        //更新蛇身位置
+        if (Segments.Count > 0)
+        {
+            UpdateSegments();
+        }
+       
+    }
+
+    private void UpdateSegments()
+    {
+        
+        for (int i = 0; i < Segments.Count; i++)
+        {
+            Segments[i].transform.position = LastPosition(i*10);
+            Segments[i].transform.eulerAngles = new Vector3(0, 0, LastDirection(i*10));
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //如果碰撞到食物，则调用Eat方法
+        if (other.CompareTag("Food"))
+        {
+            Eat();
+            Destroy(other.gameObject);
+        }
+    }
+    
+
+    //碰撞到食物时调用
+    public void Eat()
+    {
+        //生成一个新的蛇身，依据蛇尾位置，方向，生成新的蛇身
+        var tail = Instantiate(Main.Instance.SnakeSegmentPrefab,transform).GetComponent<SnakeSegment>();
+        tail.transform.ResetLocalTransform();
+        
+        tail.transform.position = LastPosition(10*(Segments.Count+1));
+        tail.transform.eulerAngles = new Vector3(0, 0, LastDirection(Segments.Count));
+        //将新的蛇身添加到蛇身列表中
+        Segments.Add(tail);
+    }
+
+    private float LastDirection(int index)
+    {
+        if (Directions.Count > index)
+        {
+            return Directions[Directions.Count-1 - index];
+        }
+        return transform.eulerAngles.z;
+    }
+
+    private Vector3 LastPosition(int index)
+    {
+        if (Positions.Count > index)
+        {
+            return Positions[Positions.Count-1 - index];
+        }
+        return transform.position;
+    }
+}
+
