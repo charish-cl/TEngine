c21e4f3a6872b985af1e7598c3fbc400b5952acb
codeelk <chaomengop@163.com>
2024-04-02 17:06:26 +0800

    去除FEngine

--- UnityProject/Assets/GameScripts/HotFix/GameLogic/System/MapSystem.cs
+++ UnityProject/Assets/GameScripts/HotFix/GameLogic/System/MapSystem.cs
@@ -1,5 +1,4 @@
 ﻿
-using FEngine;
 using TEngine;
 using UnityEngine;
 
@@ -11,42 +10,42 @@ public class MapSystem:BaseLogicSys<MapSystem>
     public Vector2Int gridSize = new Vector2Int(10,10);
     
     //寻路
-    AStarPathfinding pathfinding;
+    //AStarPathfinding pathfinding;
 
-    private Grid<AstarNode> grid => pathfinding.grid;
+    //private Grid<AstarNode> grid => pathfinding.grid;
     public override void OnStart()
     {
-        pathfinding = new AStarPathfinding(gridSize.x, gridSize.y);
+      //  pathfinding = new AStarPathfinding(gridSize.x, gridSize.y);
     }
     /// <summary>
     /// 左键添加障碍物,右键寻路
     /// </summary>
     public override void OnUpdate()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            var cell = pathfinding.grid.GetMouseTargetCell();
-            cell.isWalkable = false;
-            cell.SetColor(Color.black);
-        }
-        if (Input.GetMouseButtonDown(1))
-        {
-            var cell = pathfinding.grid.GetMouseTargetCell();
-            var result = pathfinding.FindPath(Vector2.zero, pathfinding.grid.GetCellPosition(cell.coordinate));
-                
-            for (var i = 0; i < result.Count-1; i++)
-            {
-                Debug.DrawLine(result[i],result[i+1],Color.green,1);
-            }
-        }
+        // if (Input.GetMouseButtonDown(0))
+        // {
+        //     var cell = pathfinding.grid.GetMouseTargetCell();
+        //     cell.isWalkable = false;
+        //     cell.SetColor(Color.black);
+        // }
+        // if (Input.GetMouseButtonDown(1))
+        // {
+        //     var cell = pathfinding.grid.GetMouseTargetCell();
+        //     var result = pathfinding.FindPath(Vector2.zero, pathfinding.grid.GetCellPosition(cell.coordinate));
+        //         
+        //     for (var i = 0; i < result.Count-1; i++)
+        //     {
+        //         Debug.DrawLine(result[i],result[i+1],Color.green,1);
+        //     }
+        // }
     }
 
     //通过鼠标获取落在当前格子的节点
-    public AstarNode GetMouseTargetCell()
-    {
-        return grid.GetMouseTargetCell();
-    }
-    
+    // public AstarNode GetMouseTargetCell()
+    // {
+    //     return grid.GetMouseTargetCell();
+    // }
+    //
     
     public override void OnDestroy()
     {
